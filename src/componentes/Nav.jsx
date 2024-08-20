7b13fe15230906f04ee45e586365462c5633194e
est3banst <estebeeen@outlook.com>
2024-08-20 17:05:18 -0300

    menu resp

--- src/componentes/Nav.jsx
+++ src/componentes/Nav.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
 import '../assets/Nav.css'
+import { useState } from 'react'
 
 const Nav = () => {
+  const [isVisible, setVisible] = useState(false);
+  const menuResponsive = isVisible ? 'visible' : '';
+
+  const settingVisible = () => {
+    console.log("happen")
+    setVisible(!isVisible)
+  }
   return (
+    <>
     <header className='component-container'>
       <div className='logo-container'>
         <a href="/">
@@ -10,13 +19,18 @@ const Nav = () => {
         </a> </div>
       <nav className='nav-bar'>
         <ul className='nav-bar-ul'>
-            <li>Nosotros</li>
-            <li>Trabajos</li>
-            <li>Contacto</li>
+            <li className='menu-sections'>Nosotros
+              </li>
+            <li className='menu-sections'>Trabajos
+            
+            </li>
+            <li className='menu-sections'>Contacto
+            
+            </li>
         </ul>
 
         <div className='burger-menu'>
-          <svg 
+          <svg onClick={settingVisible}
             width="46px" height="46px" viewBox="0 0 32 32" 
             fill="currentColor" x="128" y="128" role="img" 
             xmlns="http://www.w3.org/2000/svg">
@@ -26,6 +40,15 @@ const Nav = () => {
         </div>
       </nav>
     </header>
+    <div  className={`menu-responsivo ${menuResponsive}`}>
+      <a className='menu-sections' href="">nosotros <svg width="24px" height="24px" viewBox="0 0 32 32" fill="currentColor" x="128" y="128" role="img" xmlns="http://www.w3.org/2000/svg"><g fill="currentColor"><path fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m22 6l8 10l-8 10m8-10H2"/></g></svg>
+      </a>
+      <a className='menu-sections' href="">trabajos <svg width="24px" height="24px" viewBox="0 0 32 32" fill="currentColor" x="128" y="128" role="img" xmlns="http://www.w3.org/2000/svg"><g fill="currentColor"><path fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m22 6l8 10l-8 10m8-10H2"/></g></svg>
+      </a>
+      <a className='menu-sections' href="">contacto <svg width="24px" height="24px" viewBox="0 0 32 32" fill="currentColor" x="128" y="128" role="img" xmlns="http://www.w3.org/2000/svg"><g fill="currentColor"><path fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m22 6l8 10l-8 10m8-10H2"/></g></svg>
+      </a>
+    </div>
+    </>
   )
 }
 
